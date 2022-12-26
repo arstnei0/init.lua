e7a22b3fd7818d9e91ece423df16fa8169bef8ae
Zihan Chen <me@zihan.ga>
2022-12-26 10:44:30 +0800

    Update git push keymap

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,5 +2,5 @@ vim.keymap.set("n", "<leader>gr", ':Git ')
 vim.keymap.set("n", "<leader>gv", ':Git<CR>')
 vim.keymap.set("n", "<leader>gaa", ':Git add .<CR>')
 vim.keymap.set("n", "<leader>gc", ':Git commit -m ""<left>')
-vim.keymap.set("n", "<leader>gp", ':Git push')
+vim.keymap.set("n", "<leader>gp", ':Git push<CR>')
 
